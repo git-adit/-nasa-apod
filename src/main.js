a850220b717fb2d826ffed99a44b8322ae25a275
git-adit <kandasamy.adithya@gmail.com>
2026-07-26 22:14:05 -0400

    Improve styling and add new features

--- src/main.js
+++ src/main.js
@@ -1,59 +1,41 @@
 import "./style.css";
 
 const API_KEY = import.meta.env.VITE_NASA_API_KEY;
-
 const app = document.querySelector("#app");
 
-randomImage();
+loadRandomImage();
 
-function loadImage(date = "") {
+function loadRandomImage() {
   app.innerHTML = "<p>Loading...</p>";
 
-  let url = `https://api.nasa.gov/planetary/apod?api_key=${API_KEY}`;
+  fetch(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}&count=1`)
+    .then(response => response.json())
+    .then(data => {
+      const apod = data[0];
 
-  if (date) {
-    url += `&date=${date}`;
-  }
-
-  fetch(url)
-    .then((response) => response.json())
-    .then((data) => {
-      // If NASA returns a video, try another random date
-      if (data.media_type !== "image") {
-        randomImage();
+      // If NASA returns a video, try again
+      if (apod.media_type !== "image") {
+        loadRandomImage();
         return;
       }
 
       app.innerHTML = `
         <button id="randomBtn">🎲 Random Space Image</button>
 
-        <h1>${data.title}</h1>
+        <h1>${apod.title}</h1>
 
-        <p><strong>${data.date}</strong></p>
+        <p><strong>${apod.date}</strong></p>
 
-        <img src="${data.url}" alt="${data.title}">
+        <img src="${apod.url}" alt="${apod.title}" />
 
-        <p>${data.explanation}</p>
+        <p>${apod.explanation}</p>
       `;
 
       document
-        .querySelector("#randomBtn")
-        .addEventListener("click", randomImage);
+        .getElementById("randomBtn")
+        .addEventListener("click", loadRandomImage);
     })
-    .catch((err) => {
+    .catch(err => {
       app.innerHTML = `<p>Error: ${err.message}</p>`;
     });
-}
-
-function randomImage() {
-  const start = new Date("1995-06-16").getTime();
-  const end = new Date().getTime();
-
-  const randomTime = start + Math.random() * (end - start);
-
-  const randomDate = new Date(randomTime)
-    .toISOString()
-    .split("T")[0];
-
-  loadImage(randomDate);
 }
